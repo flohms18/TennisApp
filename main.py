e06788a92095472bedab3ed65e3938bb474bd2af
flohms18 <trouilletflorent@outlook.com>
2025-11-20 10:36:25 +0100

    Init

---
+++ main.py
@@ -0,0 +1,10 @@
+import random as rd
+
+def tossing():
+    print("Welcome players")
+    TH = rd.randint(0,1)
+    if TH == 0:
+        print("It's Head!")
+        print("P1! You serve! P2 You receive")
+    else:
+        print("It's Tail")
