53991eb48b93063c1dd6bc0967beda02c19eb16d
flohms18 <trouilletflorent@outlook.com>
2025-11-20 10:45:59 +0100

    Game

--- main.py
+++ main.py
@@ -9,3 +9,14 @@ def tossing():
     else:
         print("It's Tail")
         print("P2! You serve! P1 You receive")
+
+def match():
+    ScoreBoard = ["0","15","30","40","A"]
+    P1Score = 0
+    P2Score = 0
+    Point = rd.randint(0,1)
+    if Point == 0:
+        P1Score += 1
+    elif Point == 1:
+        P2Score += 1
+
